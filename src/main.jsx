77dcae094c33d7eb396cecac5abaa37e432aee24
W. Jordan Charles <williamthe5thc@icloud.com>
2024-10-24 12:43:13 -0600

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom/client'
 import { HashRouter } from 'react-router-dom'
 import App from './App'
 import './index.css'
-
+console.log("loading Main.jsx");
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <HashRouter>
