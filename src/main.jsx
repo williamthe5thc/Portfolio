ef3d04b06b0e8128a2bea98f1d10c6113fc312ce
W. Jordan Charles <williamthe5thc@icloud.com>
2024-10-24 12:53:48 -0600

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -5,6 +5,7 @@
 // import './index.css'
 console.log("loading Main.jsxs");
 ReactDOM.createRoot(document.getElementById('root')).render(
+  console.log("inside create root");
   <React.StrictMode>
     <HashRouter>
       <App />
