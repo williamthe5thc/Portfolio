5a4a69585db21d00b1e929e81206d24dd42e096f
W. Jordan Charles <williamthe5thc@icloud.com>
2024-10-26 21:49:15 -0600

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,7 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   base: '/EDPS_WilliamCharles/',
+  base: '/Portfolio/', // Make sure this matches your repo name
   resolve: {
     extensions: ['.js', '.jsx', '.json'],
   },
