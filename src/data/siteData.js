363e55bdf9b6b2e84fe4578708698470e68def49
W. Jordan Charles <williamthe5thc@icloud.com>
2024-10-28 17:44:00 -0600

    Update siteData.js

--- src/data/siteData.js
+++ src/data/siteData.js
@@ -51,7 +51,7 @@ export const projects = [
     title: "How to Effectively Date Better",
     description: "Designed a comprehensive course on effective dating using Articulate Storyline",
     tags: ["Articulate", "Instructional Design", "eLearning"],
-    image: "/images/projects/online-dating.jpg",
+    image: "/public/images/projects/online-dating.jpg",
     status: "Completed",
     date: "May 2024 - Jul 2024",
     category: "elearning"
@@ -61,7 +61,7 @@ export const projects = [
     title: "How to Learn the Waltz",
     description: "Developed a Canvas LMS course for 'The Social Dance Effect'",
     tags: ["Canvas", "LMS", "Dance Education"],
-    image: "/images/projects/how-to-waltz.jpg",
+    image: "public/images/projects/how-to-waltz.jpg",
     status: "Completed",
     date: "Aug 2023 - Dec 2023",
     category: "id"
