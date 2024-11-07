8c322b072dd552ec2677262aa664ee2205689a8e
W. Jordan Charles <williamthe5thc@icloud.com>
2024-11-07 15:20:58 -0700

    Updates

--- src/hooks/index.ts
+++
@@ -1,7 +0,0 @@
-// src/hooks/index.ts
-export * from './useFormValidation';
-export * from './useAnimationPresence';
-export * from './useModal';
-export * from './useIntersectionObserver';
-export { default as useAnalytics } from './useAnalytics';
-export * from './useAnalytics';
